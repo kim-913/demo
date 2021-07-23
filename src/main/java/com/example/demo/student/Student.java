0394b69eb3b8af48ec27377091b8bbcff26a4a0a
kim-913 <kziruo@gmail.com>
2021-07-22 19:39:54 -0700

    database postgres

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,6 +2,8 @@ package com.example.demo.student;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 // don't put @Data here because fields aren't final
 @ToString
 @Getter
@@ -9,17 +11,27 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 //@AllArgsConstructor
-
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "student_sequence",
+            strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String email;
+    private Gender gender;
+
     public Student(Long id, String name, String email, Gender gender) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.gender = gender;
     }
-
-    private Long id;
-    private String name;
-    private String email;
-    private Gender gender;
 }
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,24 +10,25 @@ import java.util.List;
 
 //expose resources clients can consumer
 @RestController
-
+@AllArgsConstructor
 //@RequestMapping(path = "api/v1/students")
 public class StudentController {
+    private final StudentService studentService;
 
     //create API end points
     @RequestMapping(path = "/api/v1/students")
     @GetMapping
     public List<Student> getAllStudents() {
-        List<Student> students = Arrays.asList(
-                new Student(1L,
-                        "Kim",
-                        "kziruo@gmail.com",
-                        Gender.MALE),
-                new Student(2L,
-                        "Tracy",
-                        "tracy@gmail.com",
-                        Gender.FEMALE)
-        );
-        return students;
+        // List<Student> students = Arrays.asList(
+        //         new Student(1L,
+        //                 "Kim",
+        //                 "kziruo@gmail.com",
+        //                 Gender.MALE),
+        //         new Student(2L,
+        //                 "Tracy",
+        //                 "tracy@gmail.com",
+        //                 Gender.FEMALE)
+        // );
+        return studentService.getAllStudents();
     }
 }
